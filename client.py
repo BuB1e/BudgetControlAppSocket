220bcd772d387a6196a7308fc48c52dfa526d0d9
Harit Sombatsiri <haritsombatsiri@gmail.com>
2024-08-08 20:55:20 +0700

    first commit

--- client.py
+++ client.py
@@ -2,6 +2,13 @@ import socket
 
 HOST = "localhost"
 PORT = 12345
+SERVER_NAME = 'servername'
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(HOST, PORT)
+client_socket.connect((HOST, PORT))
+
+sentence = input('input lowercase sentence: ')
+client_socket.send(sentence.encode())
+modified_sentence = client_socket.recv(1024)
+print('From server:', modified_sentence.decode())
+client_socket.close()
--- server.py
+++ server.py
@@ -6,5 +6,15 @@ PORT = 12345
 MAX_CLIENT = 5
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-server_socket.bind(HOST, PORT)
+server_socket.bind((HOST, PORT))
 server_socket.listen(MAX_CLIENT)
+
+print("Waiting for client . . .")
+while True:
+    connection_socket, addr = server_socket.accept()
+
+    sentence = connection_socket.recv(1024).decode()
+    capitalized_sentence = sentence.upper()
+    connection_socket.send(capitalized_sentence.encode())
+
+    connection_socket.close()
